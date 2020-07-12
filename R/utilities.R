d7704ee5a6b0baefc1ade53cdcf054353034c76c
barnzilla <j@barnzilla.ca>
2020-07-12 11:25:22 -0400

    Update validate_integer function and vignette

--- R/utilities.R
+++ R/utilities.R
@@ -672,7 +672,7 @@ validate_integer <- function(x) {
   return(
     unname(
       sapply(x, function(x) {
-        if(is.na(x) | x %% 1 > 0) {
+        if(is.na(x) | suppressWarnings(grepl("[.]", as.character(x)))) {
           NA
         } else {
           x
